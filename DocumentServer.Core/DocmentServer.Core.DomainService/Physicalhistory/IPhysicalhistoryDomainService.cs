8c9344c05e54f2c4aaf2d02c177816ad4597c8c8
knxdlp\wenlli <willieli@knx.com.cn>
2020-09-03 19:51:03 +0800

    提交IO操作

---
+++ DocumentServer.Core/DocmentServer.Core.DomainService/Physicalhistory/IPhysicalhistoryDomainService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DocmentServer.Core.DomainService.Physicalhistory
+{
+    public interface IPhysicalhistoryDomainService:Base.IBaseDomainService
+    {
+        bool Edit(DocumentServer.Core.Model.DbModel.Physicalhistory modele, IDbTransaction transaction = null);
+        /// <summary>
+        /// 获取开启的网站路径
+        /// </summary>
+        /// <returns></returns>
+        DocumentServer.Core.Model.DbModel.Physicalhistory SingleByEnable(IDbTransaction transaction = null);
+    }
+}
---
+++ DocumentServer.Core/DocmentServer.Core.DomainService/Physicalhistory/PhysicalhistoryDomainService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using DocmentServer.Core.Business.Physicalhistory;
+using DocumentServer.Core.Model.DbModel;
+
+namespace DocmentServer.Core.DomainService.Physicalhistory
+{
+    public class PhysicalhistoryDomainService : Base.BaseDomainService, IPhysicalhistoryDomainService
+    {
+        private IPhysicalhistoryBusiness business;
+        public PhysicalhistoryDomainService(IPhysicalhistoryBusiness business) : base(_business: business)
+        {
+            this.business = business;
+        }
+        public bool Edit(DocumentServer.Core.Model.DbModel.Physicalhistory modele, IDbTransaction transaction = null)
+        {
+            return this.business.Edit(modele: modele, transaction: transaction);
+        }
+
+        /// <summary>
+        /// 获取开启的网站路径
+        /// </summary>
+        /// <returns></returns>
+        public DocumentServer.Core.Model.DbModel.Physicalhistory SingleByEnable(IDbTransaction transaction = null)
+        {
+            return this.business.SingleByEnable(transaction: transaction);
+        }
+    }
+}
