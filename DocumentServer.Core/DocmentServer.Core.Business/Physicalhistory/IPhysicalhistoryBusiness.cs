0072cf12d16a5c568aaa76de5a35822403233243
knxdlp\wenlli <willieli@knx.com.cn>
2020-09-03 18:31:54 +0800

    添加文件

---
+++ DocumentServer.Core/DocmentServer.Core.Business/Physicalhistory/IPhysicalhistoryBusiness.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DocmentServer.Core.Business.Physicalhistory
+{
+    public interface IPhysicalhistoryBusiness : Base.IBaseBusiness
+    {
+        bool Edit(DocumentServer.Core.Model.DbModel.Physicalhistory modele, IDbTransaction transaction = null);
+        /// <summary>
+        /// 获取开启的网站路径
+        /// </summary>
+        /// <returns></returns>
+        DocumentServer.Core.Model.DbModel.Physicalhistory SingleByEnable(IDbTransaction transaction = null);
+    }
+}
