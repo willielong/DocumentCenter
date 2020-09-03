7d1121ec6c0dafaf0073174df6941673f72cfdc1
knxdlp\wenlli <willieli@knx.com.cn>
2020-09-03 13:38:37 +0800

    添加树

---
+++ DocumentServer.Core/DocmentServer.Core.BizService/Tree/BizTreeService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using DocmentServer.Core.DomainService.Employee;
+using DocmentServer.Core.DomainService.Organization;
+using DocmentServer.Core.DomainService.Company;
+using DocumentServer.Core.Comm;
+using DocumentServer.Core.Model.Oupt;
+
+/// <summary>
+/// 进行树形构造
+/// </summary>
+namespace DocmentServer.Core.BizService.Tree
+{
+    public class BizTreeService : BaseService.BizBaseService, IBizTreeService
+    {
+        public IEmployeeDomainService employeeDomainService { get; set; }
+        public IOrganizationDomainService organizationDomainService { get; set; }
+        public ICompanyDomainService companyDomainService { get; set; }
+        private TreeServiceExt ext;
+        public BizTreeService(IHttpContextAccessor httpContext, IDbConnection dbConnection) : base(httpContext)
+        {
+           
+        }
+
+        /// <summary>
+        /// 树形结构组装
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public IResponseMessage Trees(int type, int pid)
+        {
+            ext = new TreeServiceExt(_employeeDomainService: employeeDomainService, _organizationDomainService: organizationDomainService, _companyDomainService: companyDomainService);
+            return this.ext.JudgeTreeType(type: type, pId: pid).ToResponse();
+        }
+    }
+}
